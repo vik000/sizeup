d4cdf7a1b5ce87de99f7abb7a21e7a5db5cf54a3
vik000 <vik000@gmail.com>
2018-04-16 18:29:22 +0200

    first ajax function (unfinished)

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
+window.jQuery = window.$ = require('jquery');
 
+$( document ).ready(function() {
+    ajax_films();
+});
+
+function ajax_films(){
+  $.ajax({
+    type:"GET",
+    url:"movies/",
+    success:movies=>{
+      html=""
+      for(let film of films){
+        html += `<div class="row">
+          <div class="col-xs-12 bg-primary" style="width:50%">
+            FILM 1
+          </div>
+          <div class="col-xs-12 bg-secondary" style="width:50%">
+            FILM 2
+          </div>
+        </div>`
+      }
+    }
+
+  });
+}
